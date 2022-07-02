f6d67e31f350d205133ee6b296aaa99e9566dde2
Ráfaga <ads0118rafaga@gmail.com>
2022-07-01 21:36:08 -0300

    Feat: componentes iniciais - Curso Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <primeirocomponente />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+  import primeirocomponente from './components/primeirocomponente.vue'
+  export default{
   name: 'App',
-  components: {
-    HelloWorld
+  components:{ 
+    primeirocomponente
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/primeirocomponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Olá vue</h1>
+        <p>Meu nome é {{ nome }} e sou {{ profissão }}</p>
+    </div>
+</template>
+
+<script>
+    
+    export default{
+        name: 'primeirocomponente', 
+        data(){
+            return {
+                nome: 'Matheus',
+                profissão: 'programador'
+            }
+        }
+    }
+</script>
